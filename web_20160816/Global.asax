40061016753f5e12085058699bcd9cbf9de194eb
Hsu Ting Hua <csu70211122@gmail.com>
2016-08-16 11:34:38 +0800

    Add project files.

---
+++ web_20160816/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_20160816.MvcApplication" Language="C#" %>
---
+++ web_20160816/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_20160816.Startup))]
+namespace web_20160816
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
